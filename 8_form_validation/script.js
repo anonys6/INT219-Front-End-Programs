285676bef11780d9cfce469531e53e52d89471fc
anonys6 <shakibalam998@gmail.com>
2024-02-27 03:18:13 +0530

    done 8 form validation

--- 8_form_validation/script.js
+++
@@ -1,8 +0,0 @@
-const inputName = document.getElementById('name');
-const inputEmail = document.getElementById('email');
-const inputMessage = document.getElementById('message');
-const errorName = document.getElementById('error-name');
-const errorEmail = document.getElementById('error-email');
-const btnSubmit = document.getElementById('submit');
-
-
